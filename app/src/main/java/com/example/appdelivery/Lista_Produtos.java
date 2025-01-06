31237c5817e7833baf76f4083f2b608f316777f8
Julian-Siqueira <juliansiqueiradev@gmail.com>
2025-01-06 08:20:36 -0300

    Criando Menu e Deslogando usuario logado.

--- app/src/main/java/com/example/appdelivery/Lista_Produtos.java
+++ app/src/main/java/com/example/appdelivery/Lista_Produtos.java
@@ -1,13 +1,20 @@
 package com.example.appdelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Lista_Produtos extends AppCompatActivity {
 
@@ -16,5 +23,34 @@ public class Lista_Produtos extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_produtos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.perfil){
+            Toast.makeText(getApplicationContext(), "perfil clicado", Toast.LENGTH_SHORT).show();
+        }
+        else if (itemID == R.id.pedidos) {
+            Toast.makeText(getApplicationContext(), "pedidos clicado", Toast.LENGTH_SHORT).show();
+        }
+        else if (itemID == R.id.deslogar) {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(getApplicationContext(), "Usuario deslogado!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Lista_Produtos.this, Form_Login.class);
+            startActivity(intent);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
